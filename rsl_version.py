ec75ba6bd18a136c1ba2a6b374e5b15d006989b9
Xopar <xoparr@gmail.com>
2020-11-12 01:42:30 -0600

    Just updating with new menu for non-tournament use

--- rsl_version.py
+++ rsl_version.py
@@ -1,16 +1,15 @@
-__version__ = "5.2.117 R-1 v1"
-version_hash_1 = "Bottled Milk"
-version_hash_2 = "Bottled Milk"
+__version__ = "5.2.119 R-1 v1"
+version_hash_1 = "Longshot"
+version_hash_2 = "Longshot"
 
 
 class VersionError(Exception):
     def __init__(self, cause):
-        # message = f"Your {cause} is out of date. Please update it before continuing."
-        message = "To generate rsl tourny or qualifier seeds, you must use dev-R version 5.2.117 R-1"
+        message = f"Your {cause} is out of date. Please update it before continuing."
         with open('blind_random_settings.json', 'w') as fp:
             fp.write(message)
         super().__init__(message)
-
+        
 
 def check_rando_version():
     from version import __version__ as roman_version
@@ -20,7 +19,7 @@ def check_rando_version():
     major, minor, fix = __version__.split()[0].split('.')
     ver, sver = __version__.split()[1:]
 
-    if int(rmajor) != int(major) or int(rminor) != int(minor) or int(rfix) != int(fix):
+    if int(rmajor) != int(major) or int(rminor) != int(minor) or int(rfix) < int(fix):
         raise VersionError("dev-R or rando rando script")
-    if int(rver[2:]) != int(ver[2:]):
+    if int(rver[2:]) < int(ver[2:]):
         raise VersionError("dev-R")
