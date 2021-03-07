0381207a4736fee63128e0080e52e0573a97c991
Xopar <xoparr@gmail.com>
2021-03-06 19:02:48 -0600

    Fixed quotes to be consistent and now check that the data directory exists, if not, make it

--- random_settings_generator.py
+++ random_settings_generator.py
@@ -3,19 +3,19 @@ import rsl_tools as tools
 import Conditionals as conds
 from version import version_hash_1, version_hash_2
 tools.check_version()
-sys.path.append('randomizer')
+sys.path.append("randomizer")
 from randomizer.SettingsList import get_settings_from_tab, get_setting_info
 from randomizer.StartingItems import inventory, songs, equipment
 from randomizer.Spoiler import HASH_ICONS
 
 # Please set the weights file you with to load
-weights = 'RSL' # The default Random Settings League Season 2 weights
-# weights = 'full-random' # Every setting with even weights
-# weights = 'coop' # Uses the RSL weights with some extra modifications
-# weights = 'my_weights.json' # Provide your own weights file. If the specified file does not exist, this will create one with equal weights
-# mw_weights_file = 'rsl_multiworld.json' # If this variable exists, load this file and use it to edit loaded weights
-# mw_weights_file = 'ddr_adjustments.json'
-# mw_weights_file = 'beginner_adjustments.json'
+weights = "RSL" # The default Random Settings League Season 2 weights
+# weights = "full-random" # Every setting with even weights
+# weights = "coop" # Uses the RSL weights with some extra modifications
+# weights = "my_weights.json" # Provide your own weights file. If the specified file does not exist, this will create one with equal weights
+# mw_weights_file = "rsl_multiworld.json" # If this variable exists, load this file and use it to edit loaded weights
+# mw_weights_file = "ddr_adjustments.json"
+# mw_weights_file = "beginner_adjustments.json"
 
 COOP_SETTINGS = False # Change some settings to be more coop friendly
 STANDARD_TRICKS = True # Whether or not to enable all of the tricks in Standard settings
@@ -28,25 +28,25 @@ BROKEN_SETTINGS = [] # If any settings are broken, add their name here and they 
 
 # Handle all uncaught exceptions with logging
 def error_handler(type, value, tb):
-    with open('ERRORLOG.TXT', 'w') as errout:
+    with open("ERRORLOG.TX", 'w') as errout:
         traceback.print_exception(type, value, tb, file=errout)
         traceback.print_exception(type, value, tb, file=sys.stdout)
 sys.excepthook = error_handler
 
 
-def geometric_weights(N, startat=0, rtype='list'):
+def geometric_weights(N, startat=0, rtype="list"):
     """ Compute weights according to a geometric distribution """
-    if rtype == 'list':
+    if rtype == "list":
         return [50.0/2**i for i in range(N)]
-    elif rtype == 'dict':
+    elif rtype == "dict":
         return {str(startat+i): 50.0/2**i for i in range(N)}
 
 
 def draw_starting_item_pool(random_settings, start_with):
     """ Select starting items, songs, and equipment. """
-    random_settings['starting_items'] = draw_choices_from_pool(inventory)
-    random_settings['starting_songs'] = draw_choices_from_pool(songs)
-    random_settings['starting_equipment'] = draw_choices_from_pool(equipment)
+    random_settings["starting_items"] = draw_choices_from_pool(inventory)
+    random_settings["starting_songs"] = draw_choices_from_pool(songs)
+    random_settings["starting_equipment"] = draw_choices_from_pool(equipment)
     
     for key, val in start_with.items():
         for thing in val:
@@ -58,15 +58,15 @@ def draw_choices_from_pool(itempool):
     return random.sample(list(itempool.keys()), N)
 
 
-def generate_balanced_weights(fname='default_weights.json'):
+def generate_balanced_weights(fname="default_weights.json"):
     """ Generate a file with even weights for each setting. """
-    settings_to_randomize = list(get_settings_from_tab('main_tab'))[1:] + \
-                list(get_settings_from_tab('detailed_tab')) + \
-                list(get_settings_from_tab('other_tab')) + \
-                list(get_settings_from_tab('starting_tab'))
+    settings_to_randomize = list(get_settings_from_tab("main_tab"))[1:] + \
+                list(get_settings_from_tab("detailed_tab")) + \
+                list(get_settings_from_tab("other_tab")) + \
+                list(get_settings_from_tab("starting_tab"))
 
-    exclude_from_weights = ['bridge_tokens', 'lacs_tokens', 'triforce_goal_per_world', 'disabled_locations',
-                            'allowed_tricks', 'starting_equipment', 'starting_items', 'starting_songs']
+    exclude_from_weights = ["bridge_tokens", "lacs_tokens", "triforce_goal_per_world", "disabled_locations",
+                            "allowed_tricks", "starting_equipment", "starting_items", "starting_songs"]
     weight_dict = {}
     for name in settings_to_randomize:
         if name not in exclude_from_weights:
@@ -83,9 +83,9 @@ def generate_balanced_weights(fname='default_weights.json'):
 
 def add_standard_tricks(random_settings):
     """ Add the tricks enabled in standard to the plando. """
-    random_settings['randomize_settings'] = False
-    random_settings['disabled_locations'] = []
-    random_settings['allowed_tricks'] = ["logic_fewer_tunic_requirements", "logic_grottos_without_agony",
+    random_settings["randomize_settings"] = False
+    random_settings["disabled_locations"] = []
+    random_settings["allowed_tricks"] = ["logic_fewer_tunic_requirements", "logic_grottos_without_agony",
         "logic_child_deadhand", "logic_man_on_roof", "logic_dc_jump", "logic_rusted_switches", "logic_windmill_poh",
         "logic_crater_bean_poh_with_hovers", "logic_forest_vines", "logic_goron_city_pot_with_strength",
         "logic_lens_botw", "logic_lens_castle", "logic_lens_gtg", "logic_lens_shadow",
@@ -94,14 +94,14 @@ def add_standard_tricks(random_settings):
 
 def add_rsl_tricks(random_settings):
     """ Add some extra tricks to the plando that are beyond the scope of Standard. """
-    random_settings['allowed_tricks'] = random_settings['allowed_tricks'] + ["logic_visible_collisions", 
+    random_settings["allowed_tricks"] = random_settings["allowed_tricks"] + ["logic_visible_collisions", 
         "logic_deku_b1_webs_with_bow", "logic_lens_gtg_mq", "logic_lens_jabu_mq", "logic_lens_shadow_mq",
         "logic_lens_shadow_mq_back", "logic_lens_spirit_mq"]
 
 
 def load_weights_file(weights_fname):
     """ Given a weights filename, open it up. If the file does not exist, make it with even weights """
-    fpath = os.path.join('weights', weights_fname)
+    fpath = os.path.join("weights", weights_fname)
     try:
         with open(fpath) as fin:
             weight_dict = json.load(fin)
@@ -124,19 +124,19 @@ def main():
 
 
     # Load the weight dictionary
-    if weights in ['RSL', 'coop']:
-        weight_dict = load_weights_file('random_settings_league_s2.json')
-        if weight_dict['hash']['obj1'] != version_hash_1 or weight_dict['hash']['obj2'] != version_hash_2:
+    if weights in ["RSL", "coop"]:
+        weight_dict = load_weights_file("random_settings_league_s2.json")
+        if weight_dict["hash"]["obj1"] != version_hash_1 or weight_dict["hash"]["obj2"] != version_hash_2:
             raise VersionError("weights file")
-        weight_dict.pop('hash')
-    elif weights == 'full-random':
+        weight_dict.pop("hash")
+    elif weights == "full-random":
         weight_dict = generate_balanced_weights(None)
     else:
         weight_dict = load_weights_file(weights)
 
 
     # If a multiworld weights file is supplied, make appropriate changes
-    start_with = {'starting_items':[], 'starting_songs':[], 'starting_equipment':[]}
+    start_with = {"starting_items":[], "starting_songs":[], "starting_equipment":[]}
     if "mw_weights_file" in globals():
         mw_weights = load_weights_file(mw_weights_file)
         # Check for starting items, songs and equipment
@@ -151,16 +151,16 @@ def main():
 
 
     # If its a co-op seed, make some small changes to weights
-    if weights == 'coop':
-        weight_dict['bridge_tokens'] = {i+1: 2.0 for i in range(50)}
-        weight_dict['lacs_tokens'] = {i+1: 2.0 for i in range(50)}
-        weight_dict['mq_dungeons_random'] = {"false": 100}
-        weight_dict['mq_dungeons'] = {"0": 100,}
-        weight_dict['damage_multiplier'] = {"normal": 100}
+    if weights == "coop":
+        weight_dict["bridge_tokens"] = {i+1: 2.0 for i in range(50)}
+        weight_dict["lacs_tokens"] = {i+1: 2.0 for i in range(50)}
+        weight_dict["mq_dungeons_random"] = {"false": 100}
+        weight_dict["mq_dungeons"] = {"0": 100,}
+        weight_dict["damage_multiplier"] = {"normal": 100}
 
 
     # Check if bridge_tokens, lacs_tokens, or triforce piece count is set already, if not draw uniformly.
-    number_settings = ['bridge_tokens', 'lacs_tokens', 'triforce_goal_per_world']
+    number_settings = ["bridge_tokens", "lacs_tokens", "triforce_goal_per_world"]
     for nset in number_settings:
         if not nset in weight_dict:
             weight_dict[nset] = {i+1: 1.0 for i in range(100)}
@@ -207,12 +207,15 @@ def main():
 
     # Save the output plando
     output = {
-        'settings': random_settings,
-        'file_hash': [version_hash_1, version_hash_2, *random.choices(HASH_ICONS, k=3)]
+        "settings": random_settings,
+        "file_hash": [version_hash_1, version_hash_2, *random.choices(HASH_ICONS, k=3)]
     }
+
+    if not os.path.isdir("data"):
+        os.mkdir("data")
     with open(os.path.join("data", "random_settings.json"), 'w') as fp:
         json.dump(output, fp, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
