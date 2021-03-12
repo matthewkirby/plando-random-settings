3a93118bbe14d1418f45557ab8b69b92fc92a2b2
Xopar <xoparr@gmail.com>
2021-03-12 17:01:37 -0600

    Finished moving all customization into the weights file

--- roll_settings.py
+++ roll_settings.py
@@ -10,13 +10,6 @@ from randomizer.StartingItems import inventory, songs, equipment
 from randomizer.Spoiler import HASH_ICONS
 
 
-STANDARD_TRICKS = True # Whether or not to enable all of the tricks in Standard settings
-RSL_TRICKS = True # Add the extra tricks that we enable for rando rando
-STARTING_ITEMS = True # Draw starting items, songs, and equipment from a geometric distribution
-
-BROKEN_SETTINGS = [] # If any settings are broken, add their name here and they will be non-randomized
-
-
 def load_weights_file(weights_fname):
     """ Given a weights filename, open it up. If the file does not exist, make it with even weights """
     fpath = os.path.join("weights", weights_fname)
@@ -78,25 +71,6 @@ def draw_choices_from_pool(itempool):
     return random.sample(list(itempool.keys()), N)
 
 
-def add_standard_tricks(random_settings):
-    """ Add the tricks enabled in standard to the plando. """
-    random_settings["randomize_settings"] = False
-    random_settings["disabled_locations"] = []
-    random_settings["allowed_tricks"] = ["logic_fewer_tunic_requirements", "logic_grottos_without_agony",
-        "logic_child_deadhand", "logic_man_on_roof", "logic_dc_jump", "logic_rusted_switches", "logic_windmill_poh",
-        "logic_crater_bean_poh_with_hovers", "logic_forest_vines", "logic_goron_city_pot_with_strength",
-        "logic_lens_botw", "logic_lens_castle", "logic_lens_gtg", "logic_lens_shadow",
-        "logic_lens_shadow_back", "logic_lens_spirit"]
-
-
-def add_rsl_tricks(random_settings):
-    """ Add some extra tricks to the plando that are beyond the scope of Standard. """
-    random_settings["allowed_tricks"] = random_settings["allowed_tricks"] + ["logic_visible_collisions", 
-        "logic_deku_b1_webs_with_bow", "logic_lens_gtg_mq", "logic_lens_jabu_mq", "logic_lens_shadow_mq",
-        "logic_lens_shadow_mq_back", "logic_lens_spirit_mq"]
-
-
-
 def generate_plando(weights, override_weights_fname):
     # Load the weight dictionary
     if weights == "RSL":
@@ -122,12 +96,31 @@ def generate_plando(weights, override_weights_fname):
         # Replace the options
         if override_options is not None:
             for key, value in override_options.items():
-                if not key.startswith("extra_"): # Overwrite
+                # Handling overwrite
+                if not (key.startswith("extra_") or key.startswith("remove_")):
                     weight_options[key] = value
-                elif key.split("extra_")[1] not in weight_options: # If its extra but key isnt there
-                    weight_options[key.split("extra_")[1]] = value
-                else: # Both existing options and extra options
-                    weight_options[key.split("extra_")[1]].update(value) 
+                    continue
+
+                # Handling extras
+                if key.startswith("extra_"):
+                    option = key.split("extra_")[1]
+                    if option not in weight_options:
+                        weight_options[option] = value
+                    else: # Both existing options and extra options
+                        if isinstance(weight_options[option], dict):
+                            weight_options[option].update(value) 
+                        else:
+                            weight_options[option] += value
+                            weight_options[option] = list(set(weight_options[option]))
+
+                # Handling removes
+                if key.startswith("remove_"):
+                    print("removing", key)
+                    option = key.split("remove_")[1]
+                    if option in weight_options:
+                        for item in value:
+                            if item in weight_options[option]:
+                                weight_options[option].remove(item)
 
         # Replace the weights
         for key, value in override_weights.items():
@@ -152,21 +145,16 @@ def generate_plando(weights, override_weights_fname):
         random_settings[setting] = random.choices(list(options.keys()), weights=list(options.values()))[0]
 
 
-    # Check conditional settings for RSL
-    if weight_options is not None and "conditionals" in weight_options:
-        conds.parse_conditionals(weight_options["conditionals"], weight_dict, random_settings)
-
-
-    # Add the tricks to the plando
-    if STANDARD_TRICKS:
-        add_standard_tricks(random_settings)
-    if RSL_TRICKS:
-        add_rsl_tricks(random_settings)
-
-
-    # Draw the starting items, songs, and equipment
-    if STARTING_ITEMS:
-        draw_starting_item_pool(random_settings, start_with)
+    # Add starting items, conditionals, tricks and excluded locations
+    if weight_options is not None:
+        if "conditionals" in weight_options:
+            conds.parse_conditionals(weight_options["conditionals"], weight_dict, random_settings)
+        if "tricks" in weight_options:
+            random_settings["allowed_tricks"] = weight_options["tricks"]
+        if "disabled_locations" in weight_options:
+            random_settings["disabled_locations"] = weight_options["disabled_locations"]
+        if "starting_items" in weight_options and weight_options["starting_items"] == True:
+            draw_starting_item_pool(random_settings, start_with)
 
 
     # Format numbers and bools to not be strings
