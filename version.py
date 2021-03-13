a6a8289756337927b96def91b30c1b388dc3166a
Xopar <xoparr@gmail.com>
2021-03-13 16:15:07 -0600

    Final weight change for s3 and version update

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 __version__ = "2.0"
-version_hash_1 = "Longshot"
-version_hash_2 = "Longshot"
+version_hash_1 = "Map"
+version_hash_2 = "Map"
 
 randomizer_version = '6.0.36 R-1'
 randomizer_commit = '26c70bd172aaebe9c90820d60c950071b5691f3e'
