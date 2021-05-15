5162374feec22f50d5a82935c396d2a41982c0c3
Xopar <xoparr@gmail.com>
2021-05-15 10:44:23 -0500

    Randomizer version update

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
-__version__ = "2.0.3"
-version_hash_1 = "Map"
-version_hash_2 = "Map"
+__version__ = "2.0.4"
+version_hash_1 = "Cucco"
+version_hash_2 = "Cucco"
 
-randomizer_version = '6.0.41 R-1'
-randomizer_commit = 'd6755f8e4b1c6b5d5612e583f8f91fa303f1669b'
+randomizer_version = '6.0.57 R-1'
+randomizer_commit = 'eaa980ccb03b8de148c23bf835252c1c0339cd4d'
