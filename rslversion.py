45ad67ba6dcaf92af8cee46308b8b3a608f332f9
Xopar <xoparr@gmail.com>
2023-07-18 15:29:19 -0500

    Randomizer version bump and tcg key bugfix

--- rslversion.py
+++ rslversion.py
@@ -1,4 +1,4 @@
-__version__ = "2.5.7"
+__version__ = "2.5.8"
 
-randomizer_version = '7.1.143 R-1'
-randomizer_commit = '06390ece7e38fce1dd02ca60a28a7b1ff9fceb10'
+randomizer_version = '7.1.154 R-1'
+randomizer_commit = 'c5af1e9df2dd280b70b25f02d80d13dce9680d58'
