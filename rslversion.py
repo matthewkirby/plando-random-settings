248f8b53f4a3095ff4206b41759793f8813e7bdd
Matthew Kirby <matthewkirby@email.arizona.edu>
2023-08-27 14:48:23 -0400

    Randomizer version bump

--- rslversion.py
+++ rslversion.py
@@ -1,4 +1,4 @@
-__version__ = "2.5.9"
+__version__ = "2.5.10"
 
-randomizer_version = '7.1.154 R-1'
-randomizer_commit = 'c5af1e9df2dd280b70b25f02d80d13dce9680d58'
+randomizer_version = '7.1.181 R-1'
+randomizer_commit = '4dbfdae3b8d1242af895569feb06536581dcf2b6'
