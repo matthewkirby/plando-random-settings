4a9c3e4a68ab9929c1fe96846c1bc50c57124042
Xopar <xoparr@gmail.com>
2025-02-03 13:23:49 -0600

    Randomizer version bump

--- rslversion.py
+++ rslversion.py
@@ -1,9 +1,9 @@
-__version__ = "2.8.0"
+__version__ = "2.8.1"
 
 # Randomizer Info
 randomizer_repo = 'rrealmuto/OoT-Randomizer'
-randomizer_version = '8.2.50 Rob-128'
-randomizer_commit = '2030053144ce99f3364244117db64ba9585a1513'
+randomizer_version = '8.2.52 Rob-132'
+randomizer_commit = '464b5184646694ec422a7dfe2a757725d73a7e1b'
 
 # Requirements
 MIN_PY_VERSION = (3, 9)
