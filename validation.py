ec77379ccdff35862783b5c519918b6ddaa1cc88
Xopar <xoparr@gmail.com>
2026-06-08 11:38:23 -0500

    Validation for override files

--- validation.py
+++ validation.py
@@ -1,4 +1,6 @@
 import ast
+import os
+import json
 
 from multiselects import ms_option_lookup
 from randomizer.SettingsList import SettingInfos
@@ -12,7 +14,7 @@ def validate_rsl(weights, verbose=False):
     validate_overrides()
 
 
-def validate_weights(weights):
+def validate_weights(weights, check_existing=True, check_missing=True, print_headings=True):
     """ Function to check for new settings and options when the randomizer is updated. """
     # Settings to not care about validating if they show up in balanced weights because they are handled manually
     ignore_settings = ["custom_ice_trap_percent", "custom_ice_trap_count", "hint_dist"] + list(ms_option_lookup.keys())
@@ -22,33 +24,40 @@ def validate_weights(weights):
     randomizer_settings = rs.generate_balanced_weights(None)
 
     # Find new or changed settings by name
-    print("Checking for new or removed settings...")
+    changed_settings = []
+    if print_headings:
+        print("Checking for new or removed settings...")
     rsl_settings_set = set(weights.keys())
     ootr_settings_set = set(randomizer_settings.keys())
-    for setting in rsl_settings_set-ootr_settings_set:
-        print(f"\tRSL:  {setting} {list(weights[setting].keys())}")
-    for setting in ootr_settings_set-rsl_settings_set:
-        if setting not in ignore_settings:
-            print(f"\tOoTR: {setting} {list(SettingInfos.setting_infos[setting].choice_list)}")
-    print("\n\n")
+    if check_existing: # Check settings in weights file still exist
+        for setting in rsl_settings_set-ootr_settings_set:
+            changed_settings.append(f"\tRSL:  {setting} {list(weights[setting].keys())}")
+    if check_missing: # Check OoTR Settings to find anything missing in weights
+        for setting in ootr_settings_set-rsl_settings_set:
+            if setting not in ignore_settings:
+                changed_settings.append(f"\tOoTR: {setting} {list(SettingInfos.setting_infos[setting].choice_list)}")
+    for line in changed_settings:
+        print(line)
 
     # Find new or changed options
-    print("Checking settings for new or removed options...")
+    if print_headings:
+        print("\nChecking settings for new or removed options...")
     for setting, optweights in weights.items():
         if setting in ignore_settings:
             continue
         rsl_options = set(optweights.keys())
         ootr_options = set(map(lambda x: str(x).lower(), SettingInfos.setting_infos[setting].choice_list))
-        for option in rsl_options-ootr_options:
-            print(f"\tRSL:  {setting}:{option} removed or renamed")
-        for option in ootr_options-rsl_options:
-            if setting not in ignore_options:
-                print(f"\tOoTR: {setting}:{option} newly added")
-    print("\n\n")
+        if check_existing: # Only check that the options in the weights still exist
+            for option in rsl_options-ootr_options:
+                print(f"\tRSL:  {setting}:{option} removed or renamed")
+        if check_missing: # Only check that the weight has all options for each setting
+            for option in ootr_options-rsl_options:
+                if setting not in ignore_options:
+                    print(f"\tOoTR: {setting}:{option} newly added")
 
 
 def validate_multiselect_knowledge():
-    print("Verifying multiselect knowledge...")
+    print("\nVerifying multiselect knowledge...")
     for mskey in ms_option_lookup.keys():
         rslset = set(ms_option_lookup[mskey])
         ootrset = set(SettingInfos.setting_infos[mskey].choice_list)
@@ -56,12 +65,20 @@ def validate_multiselect_knowledge():
             print(f"\t{mskey} mismatched!")
             print(f"\t\tRSL:  {rslset-ootrset}")
             print(f"\t\tOoTR: {ootrset-rslset}")
-    print("\n\n")
 
 
 
 def validate_overrides():
-    pass
+    print("\nValidating override files...")
+    override_dir = "weights"
+    for fname in os.listdir(override_dir):
+        if fname.endswith(".json") and fname != "rsl_main.json":
+            print(f"  > {fname}")
+            with open(os.path.join(override_dir, fname)) as f:
+                data = json.load(f)
+            if "weights" in data:
+                validate_weights(data["weights"], check_missing=False, print_headings=False)
+
 
 
 
@@ -118,7 +135,7 @@ def summarize_conditionals():
 
 
 def validate_conditionals(verbose):
-    print("Validating dictionary keys in conditionals...")
+    print("\nValidating dictionary keys in conditionals...")
     results = summarize_conditionals()
     for funcname, visitor in results.items():
         print(f"  > {funcname} ".ljust(64, '='))
